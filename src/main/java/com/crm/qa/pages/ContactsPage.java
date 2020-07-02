ce61c596e41ac70946b263cbb9c1f368a2172447
preethagit <preetha.mathews1@gmail.com>
2020-07-02 21:13:07 +1000

    added steps to enter name in contactpage

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -16,23 +16,13 @@ public class ContactsPage extends TestBase{
 	@FindBy(xpath = "//td[contains(text(),'Contacts')]")
 	WebElement contactsText;
 	
-	@FindBy(name="title")
-	WebElement titleDropdown;
 	
-	
-	@FindBy(id="first_name")
-	WebElement firstName;
-	
-	@FindBy(id="surname")
-	WebElement lastName;
-	
-	@FindBy(name="client_lookup")
-	WebElement company;
-	
-	@FindBy(xpath = "//form[@id='contactForm']/table/tbody/tr/td/input[@type='submit' and @value='Save']")
-	WebElement saveButton;
-	
+	@FindBy(name="cs_name")
+	WebElement searchName;
 		
+	@FindBy(name="cs_submit")
+	WebElement searchButton;
+	
 	public ContactsPage(){
 	PageFactory.initElements(driver, this);				
 	}
@@ -44,22 +34,22 @@ public class ContactsPage extends TestBase{
 		return contactsText.isDisplayed();
 	}
 	
+	
 	public void selectContactByName(String name)
 	{
 		String nameXpath = "//a[text()='"+ name +"']//parent::td[@class='datalistrow']/preceding-sibling::td/input[@name='contact_id']";
 		driver.findElement(By.xpath(nameXpath)).click();
 	}
 	
-	public void createNewContact(String title, String firstnm, String Lastnm, String companynm)
+	public void enterName(String name)
 	{
-		Select titleObj = new Select(driver.findElement(By.name("title")));
-	//	Select titleObj = new Select(titleDropdown);
-		
-		titleObj.selectByValue(title);
-		titleObj.selectByVisibleText(title);
-		firstName.sendKeys(firstnm);
-		lastName.sendKeys(Lastnm);
-		company.sendKeys(companynm);
-		saveButton.click();
+		searchName.sendKeys(name);
 	}
-}
+	
+	public void clickSearch()
+	{
+		searchButton.click();
+		
+	}
+	
+	}
